11406e10a673b7afbba0385a71d0c190ad28e667
lilyyuan <allmyinsanityinaname@gmail.com>
2014-09-07 00:41:37 -0400

    zipcode adding html elements

--- js/functionality.js
+++ js/functionality.js
@@ -1,8 +1,8 @@
 var zipCode = "";
-/*
+
 function findWeather(){
-	$("searchBox").click(function(random){ //button for clicking to next page
-		zipCode = $("zipCodeText").text(); //stuff inside the box to enter zip code
+	$("zipcode-button").click(function(random){ //button for clicking to next page
+		zipCode = $("zipcode-data").text(); //stuff inside the box to enter zip code
 		random.jsonp({
  			url : "http://api.wunderground.com/api/d17f7b9a0dddc27a/geolookup/q/"+zipCode+".json",
   			success : function(parsed_json) {
@@ -13,14 +13,5 @@ function findWeather(){
   		});
 	})
 }
-*/
 
-		random.jsonp({
- 			url : "http://api.wunderground.com/api/d17f7b9a0dddc27a/geolookup/q/10003.json",
-  			success : function(parsed_json) {
-  				var location = parsed_json['location']['city'];
-  				var temp_f = parsed_json['current_observation']['temp_f'];
-  				alert("Current temperature in " + location + " is: " + temp_f);
-  			}
-  		});
 
