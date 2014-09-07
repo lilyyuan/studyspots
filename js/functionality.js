311b4bca697da4fe5814a30f67792d9c4c4dfbc0
lilyyuan <allmyinsanityinaname@gmail.com>
2014-09-07 01:30:40 -0400

    finished sending zipcode

--- js/functionality.js
+++ js/functionality.js
@@ -1,11 +1,13 @@
 var zipCode = "";
 
 function findWeather(){
-	$("zipcode-button").click(function(random){ //button for clicking to next page
-		zipCode = $("zipcode-data").text(); //stuff inside the box to enter zip code
-		random.jsonp({
- 			url : "http://api.wunderground.com/api/d17f7b9a0dddc27a/geolookup/q/"+zipCode+".json",
-  			success : function(parsed_json) {
+	$("#zipcode-button").click(function(){ 
+		zipCode = $("#zipcode-data").val(); 
+		console.log(zipCode);
+		$.ajax({
+  			url : "http://api.wunderground.com/api/d17f7b9a0dddc27a/conditions/geolookup/q/"+zipCode+".json",
+ 			 dataType : "jsonp",
+ 			 success : function(parsed_json) {
   				var location = parsed_json['location']['city'];
   				var temp_f = parsed_json['current_observation']['temp_f'];
   				alert("Current temperature in " + location + " is: " + temp_f);
@@ -14,4 +16,6 @@ function findWeather(){
 	})
 }
 
-
+$( document ).ready(function() {
+  findWeather();
+});
